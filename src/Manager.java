5492333d085d367997f1e649728e9cca85133327
Serh346 <sterniichuk.serhii@gmail.com>
2021-09-27 00:57:13 +0300

    Serhii`s commit
    FileManager - new class for saving results of game
    in FileManager was modified logic. Added 2, 3 and 4th items.
    in Menu created class item, because it can be available and unavailable, it has
     name and number.
    Classes Student and Person were implemented by "Serializable" for using in binary files. Other properties of these classes were not modified.
    Test results:
    questions are not changing.

--- src/Manager.java
+++ src/Manager.java
@@ -1,43 +1,86 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class Manager {
     private static boolean inGame = false;
-
+    private static Menu menu;
     private static Student player;
     private static Map gameMap = new Map();
+    private static Vector<Student> PlayersList;
+    private int item;
+    private final Scanner in = new Scanner(System.in);
 
-    public static boolean game() {
-        if (!inGame) {
-            Menu.hello();
-            int range =  Menu.printStartMenu();
-            int item = Menu.getMenuItem(range);
-
-            if(item == 1){
-                player = Game.createStudent();
-                gameMap.generateMissions(player);
-                inGame = true;
-                gameMap.openMission();
-            }
-            else if(item == 4) return false;
-            else System.out.println("Sorry, I haven't finished writing the code yet");
+    public Manager() throws FileNotFoundException {
+        PlayersList = FileManager.init_old_Players();
+        menu = new Menu();
+        if(PlayersList == null){
+            PlayersList = new Vector<>(5);
+            menu.continue_game.available = false;
+            menu.score.available = false;
         }
-
+    }
+    public void start() throws FileNotFoundException {
+        inGame = true;
+        Menu.hello();
         while (inGame) {
-            //System.out.println("In Game");
+            inMainMenu();
             if(player.getMana() <= 0){
                 System.out.println("YOU DIED");
-                return false;
+            }
+        }
+    }
+    private boolean inGameMenu(){
+        int range = Menu.printGameMenu();
+        int item = Menu.getMenuItem(range);
+        player.info();
+        if(item == 1) gameMap.openMission();
+        if (item == 2) return false;
+        return true;
+    }
+    private void inMainMenu(){
+        int range =  Menu.printMainMenu();
+        int item = Menu.getMenuItem(range);
+        if(item == 1){
+            player = Game.createStudent();
+            inGame = true;
+            PlayersList.add(player);
+        }
+        else if(item == 2){
+            System.out.println("\nPlease, select your account");
+            for (int i = 0; i < PlayersList.size(); ++i) {
+                Student one = PlayersList.elementAt(i);
+                System.out.println("#" + (i + 1) + " Name: " + one.getName() + "score: " + one.getLevel());
+            }
+            System.out.println("Back to menu: 0");
+            int choice = -1;
+            while(choice < 0 || choice > PlayersList.size()){
+                System.out.println("Enter: ");
+                choice = in.nextInt();
             }
 
-            player.info();
-            int range = Menu.printGameMenu();
-            int item = Menu.getMenuItem(range);
+            if(choice == 0){
+                item = 0;
+            }else {
+                player = PlayersList.elementAt(choice - 1);
+            }
 
-            if(item == 1) gameMap.openMission();
-            if(item == 2) System.out.println("Save game");
-            if (item == 3) return false;
         }
-
-        return true;
+        else if(item == 3){
+            System.out.println("Your score is " + player.score);
+        }
+        else if(item == 4){
+            inGame = false;
+            FileManager.saveGame(PlayersList);
+            return;
+        }
+        if((item == 1) || (item == 2)){
+            menu.continue_game.available = true;
+            menu.score.available = true;
+            gameMap.generateMissions(player);
+            gameMap.openMission();
+            inGameMenu();
+        }
     }
 }
