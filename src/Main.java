12dc6f3ffc61ed1d21dba2ca9f49263843088a7f
danilrevyakin <90598249+danilrevyakin@users.noreply.github.com>
2021-09-27 19:58:02 +0300

    First commit
    Test Git

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+        System.out.print("Hello, world!");
         Manager manager = new Manager();
         manager.start();
     }
