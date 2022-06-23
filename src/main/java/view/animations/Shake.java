19e448e558f7c98df17abb4ee5f0d8f2c519acfd
danilrevyakin <90598249+danilrevyakin@users.noreply.github.com>
2022-06-23 20:34:27 +0300

    Authorization/Registration GUI is implemented

---
+++ src/main/java/view/animations/Shake.java
@@ -0,0 +1,21 @@
+package view.animations;
+
+import javafx.animation.TranslateTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+public class Shake {
+    private TranslateTransition tt;
+
+    public Shake(Node node){
+        tt = new TranslateTransition(Duration.millis(70), node);
+        tt.setFromX(0f);
+        tt.setByX(10f);
+        tt.setCycleCount(3);
+        tt.setAutoReverse(true);
+    }
+
+    public void playAnim(){
+        tt.playFromStart();
+    }
+}
